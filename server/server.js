b85b2550159ae6bccf0d15bd7e0e1ac7f91b02f3
Sanyat <sanyat.hoque@gmail.com>
2021-02-24 17:22:15 +0600

    added envs

--- server/server.js
+++ server/server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Message = require('./message');
+const Messages = require('./message');
 const socket = require('socket.io');
 const path = require('path');
-
+require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 // Assign the value of your mongoDB connection string to this constant
-const dbConnectString = "mongodb+srv://Sanyat:Sanyat1234@cluster0.r6sod.mongodb.net/Realchatdb?retryWrites=true&w=majority";
-
+// const dbConnectString = "mongodb+srv://Sanyat:Sanyat1234@cluster0.r6sod.mongodb.net/Realchatdb?retryWrites=true&w=majority";
+const db_mongo_atlas_compass = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.r6sod.mongodb.net/samplename?retryWrites=true&w=majority`;
 // Updating mongoose's promise version
 mongoose.Promise = global.Promise;
 
 // Connecting to MongoDB through Mongoose
-mongoose.connect(dbConnectString).then(() => {
+mongoose.connect(db_mongo_atlas_compass).then(() => {
     console.log('connected to the db');
 }).catch((err) => {
     console.log(err);
@@ -33,7 +35,7 @@ app.use(bodyParser.json());
 
 // GET all the previous messages
 app.get('/api/message', (req, res) => {
-    Message.find({}).exec((err, messages) => {
+    Messages.find().exec((err, messages) => {
         if(err) {
             res.send(err).status(500);
         } else {
@@ -41,10 +43,30 @@ app.get('/api/message', (req, res) => {
         }
     });
 });
-
+// // GET all the previous messages
+// app.get('/api/message/:postId', (req, res) => {
+//     Messages.findById(req.params.postId).exec((err, messages) => {
+//         if(err) {
+//             res.send(err).status(500);
+//         } else {
+//             res.send(messages).status(200);
+//         }
+//     });
+// });
+// GET all the previous messages
+// app.get('/api/message/:postId', (req, res) => {
+//     Messages.find({street_no: req.params.postId}).exec((err, messages) => {      //"street_no":"12"      "Street": "Belcourt"  findById
+//         if(err) {
+//             res.send(err).status(500);
+//         } else {   
+//             res.send(messages).status(200);
+//             console.log(messages);
+//         }
+//     });
+// });
 // POST a new message
 app.post('/api/message', (req, res) => {
-    Message.create(req.body).then((message) => {
+    Messages.create(req.body).then((message) => {
         res.send(message).status(200);
     }).catch((err) => {
         console.log(err);
